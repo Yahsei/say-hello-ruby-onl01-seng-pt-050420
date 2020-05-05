3c3f922f1df89d27b397379002a8fdf1ff7b8464
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-05 01:46:30 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,11 @@
 # Build your say_hello method here
+
+require_relative './spec_helper'
+
+describe "say_hello" do 
+
+  it 'defaults to Ruby Programmer when no name is passed in' do
+    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
+    say_hello()
+  end
+end
